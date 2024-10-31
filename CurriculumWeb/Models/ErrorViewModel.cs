9e69bc9055b70671bcf853ebf9220420c590485d
mubdias <murilo_barberini@hotmail.com>
2024-10-31 17:57:25 -0300

    Inicial commit

---
+++ CurriculumWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CurriculumWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
